b9a8b43b92551439f33aad100a9686a6668e4e28
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-06 12:00:09 +0200

    Add docstring

--- src/cmip7_scenariomip_ghg_generation/input4mips_cvs_helpers.py
+++ src/cmip7_scenariomip_ghg_generation/input4mips_cvs_helpers.py
@@ -5,7 +5,23 @@ Helpers for generating input4MIPs CVs information
 
 def create_source_id(esgf_institution_id: str, cmip_scenario_name: str, esgf_version: str) -> str:
     """
-    TODO: docstring
+    Create source ID from component parts
+
+    Parameters
+    ----------
+    esgf_institution_id
+        Institution ID to use on ESGF
+
+    cmip_scenario_name
+        Name of the CMIP scenario
+
+    esgf_version
+        Version to use for the dataset on ESGF
+
+    Returns
+    -------
+    :
+        Generated source ID
     """
     source_id = f"{esgf_institution_id}-{cmip_scenario_name}-{esgf_version.replace('.', '-')}"
 
