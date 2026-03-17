9410806bb1d6d330bdd69c9766bec2182ed39bd9
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2026-03-17 21:02:55 +1100

    Add file including extensions

--- scripts/create-latest-set-of-concentration-files.sh
+++ scripts/create-latest-set-of-concentration-files.sh
@@ -20,4 +20,5 @@ pixi run python scripts/generate-concentration-files.py \
     --n-workers-multiprocessing 8 \
     --n-workers-multiprocessing-magicc 2 \
     --n-workers-per-magicc-notebook 6 \
-    --emissions-file data/raw/input-scenarios/202601301330_202512071232_202511040855_202511040855_complete-emissions.csv
+    --emissions-file data/raw/input-scenarios/202603081555_202512071232_202511040855_202511040855_complete-emissions.csv
+# --emissions-file data/raw/input-scenarios/202601301330_202512071232_202511040855_202511040855_complete-emissions.csv
