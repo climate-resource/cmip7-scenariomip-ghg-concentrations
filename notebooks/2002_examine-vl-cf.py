2a8fce9214c05946487bae1a25138e317a5e07ba
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2026-07-23 17:23:29 +1000

    Check

--- notebooks/2002_examine-vl-cf.py
+++ notebooks/2002_examine-vl-cf.py
@@ -33,7 +33,7 @@ import xarray as xr
 # %%
 # Double check these values before running
 output_bundle = "1.1.0"
-output_bundle = "dev-test"
+# output_bundle = "dev-test"
 local_out_root = Path(f"../output-bundles/{output_bundle}/data/processed/esgf-ready/")
 compare_to_esgf_version = "1.1.0"
 compare_to_esgf_version_history_source_id = "CR-CMIP-1-0-0"
